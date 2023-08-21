3c8b31f5dc83ec256b0bd9a610fa1b53c1fe8c9f
Zackrey Bichsel <biksel09@hotmail.com>
2023-08-20 19:32:26 -0500

    erase and restart with index.js

--- index.js
+++ index.js
@@ -1,5 +1,68 @@
-const CLI = require('./lib/cli.js');
+// importing necessities 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Triangle, Square, Circle } = require('./lib/shapes');
+const SVG = require('./lib/svg');
 
-const cli = new CLI();
+//questions prompt
+function questions() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'How would you like your logo to read?',
+                name: 'text',
+            },
+            {
+                type: 'input',
+                message: 'What color would you like the logo to be? (Enter color keyword or hexadecimal value)',
+                name: 'textColor',
+            },
+            {
+                type: 'list',
+                message: 'Which shape will you choose?',
+                name: 'shape',
+                choices: ['Triangle', 'Square', 'Circle'],
+            },
+            {
+                type: 'input',
+                message: 'Which color would you like the logo to be? (Enter color keyword or hexadecimal value)',
+                name: 'shapeColor',
+            },
+        ])
+        .then((response) => {
+            generateSVG(response)
 
-cli.run();
+            if (response.text.length > 3) {
+                console.log('No more than 3 characters... Please try again!');
+                questions();
+            } else {
+                writeToFile('logo.svg', svgString)
+            }
+        });
+};
+
+let svgString = "";
+
+function generateSVG(response) {
+    let shapeOption;
+
+    if (response.shape === 'Triangle') {
+        shapeOption = new Triangle();
+    } else if (response.shape === 'Square') {
+        shapeOption = new Square()
+    } else if (response.shape === 'Circle') {
+        shapeOption = new Circle()
+    }
+    shapeOption.setColor(response.shapeColor);
+
+    const svg = new SVG();
+    svg.setTextString(shapeOption)
+    svgString = svg.render();
+}
+
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, (err) => err ? console.log(err) : console.log('Generated logo.svg!'));
+};
+
+questions()
